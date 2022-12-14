acee4661d75d3268d543bedc7fe1cc354c3fca25
João Manoel de Almeida Lima <83477417+JoaoManoelVK@users.noreply.github.com>
2022-12-14 09:55:30 -0300

    Teste

--- src/4-atividade-ddd/API/src/create/CreateRoomIn.ts
+++ src/4-atividade-ddd/API/src/create/CreateRoomIn.ts
@@ -1,12 +1,6 @@
-<<<<<<<< HEAD:src/4-atividade-ddd/API/src/create/CreateRoomIn.ts
-import { Room } from "../../../../4-atividade-ddd/core/entity/Room"
-import { Player } from "../../../../4-atividade-ddd/core/entity/Player"
-========
 import { Player } from "../../../core/entity/Player"
 import { Room } from "../../../core/entity/Room"
->>>>>>>> 9f2f71509f3c3c2faa437231c8ef4b8628db3cc3:src/4-atividade-ddd/app/room/create/CreateRoomIn.ts
 import AcessConfig, { Visibility } from "../../../core/valueObjects/AcessConfig"
-
 export class CreateRoomIn {
     nickName: string
     visibility: Visibility
