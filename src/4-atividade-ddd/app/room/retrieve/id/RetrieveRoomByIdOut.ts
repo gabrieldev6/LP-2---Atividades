9f2f71509f3c3c2faa437231c8ef4b8628db3cc3
gabrieldev6 <gabriel.christ.silva@gmail.com>
2022-12-13 21:03:32 -0300

    retrieveRoomByIdOut

--- src/4-atividade-ddd/app/room/retrieve/id/RetrieveRoomByIdOut.ts
+++ src/4-atividade-ddd/app/room/retrieve/id/RetrieveRoomByIdOut.ts
@@ -1,10 +1,20 @@
 import AcessConfig, { Visibility } from "../../../../core/valueObjects/AcessConfig";
-
+import {Room} from "../../../../core/entity/Room"
 export class RetrieveRoomByIdOut {
-    private
-    
+    private link: String
+    private status: String
+    private visibility: Visibility
     constructor(link:String, status: String, visibility: Visibility) {
         this.link = link
-        this.
+        this.status = status
+        this.visibility = visibility
+    }
+
+    static RetrieveRoomByIdOut(aRoom: Room) {
+        return new RetrieveRoomByIdOut(
+            aRoom.getRoomLink().getValue(),
+            aRoom.getStatus().toString(),
+            aRoom.getAcessConfig().getVisibility()
+        )
     }
 }
